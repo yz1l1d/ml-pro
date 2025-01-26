e20bdcc9413c93d821d2b88a7d90cd6fe66cab28
Yubaraj Singh <yubarajsingha2808@gmail.com>
2025-01-26 14:39:15 +0530

    Data_ingestion

--- src/exception.py
+++ src/exception.py
@@ -19,10 +19,5 @@ class CustomException(Exception):
         return self.error_message
     
 
-    if __name__ =="__main__":
-        try:
-            a=1/0
-        except:
-            logging.info("Divided by zero")
-            raise CustomException
+   
 
