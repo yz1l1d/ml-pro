9df29f95b660404b162eb08a377f722ebd74ab2e
Yubaraj Singh <yubarajsingha2808@gmail.com>
2025-02-06 19:38:00 +0530

    Prediction Pipeline

--- app.py
+++ app.py
@@ -18,24 +18,24 @@ def index():
 
 @app.route('/predictdata', methods = ['GET','POST'])
 def predict_datapoint():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('home.html')
     else:
         data = CustomData(
-            gender= request.form.get('gender'),
-            race_ethnicity= request.form.get('ethnicity'),
+            gender = request.form.get('gender'),
+            race_ethnicity = request.form.get('ethnicity'),
             parental_level_of_education = request.form.get('parental_level_of_education'),
             lunch = request.form.get('lunch'),
             test_preparation_course = request.form.get('test_preparation_course'),
-            reading_score = request.form.get('reading_score'),
-            writing_score = request.form.get('writing_score'),
+            reading_score = float(request.form.get('reading_score')),
+            writing_score = float(request.form.get('writing_score')),
         )
         pred_df = data.get_data_as_data_frame()
         print(pred_df)
 
         predict_pipeline = PredictPipelne()
-        result = predict_pipeline.predict(pred_df)
-        return render_template('home.html', result = result[0])
+        results = predict_pipeline.predict(pred_df)
+        return render_template('home.html', results = results[0])
     
 
 if __name__ == "__main__":
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -12,9 +12,9 @@ class PredictPipelne:
         try:
 
             model_path = 'artifacts\model.pkl'
-            preprocessor_path =  'artifacts\preprocessor.pkl'
+            preprocessor_path =  'artifacts\proprocessor.pkl'
             model = load_object(file_path = model_path)
-            preprocessor = load_object(file_path = model_path)
+            preprocessor = load_object(file_path = preprocessor_path)
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
             return preds
